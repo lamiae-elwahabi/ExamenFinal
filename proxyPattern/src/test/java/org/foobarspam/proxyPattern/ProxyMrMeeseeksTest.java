ec27ce2b08495c4fa57730c01d37ae78d74007ca
lamiae-elwahabi <lamiaewahabi@hotmail.es>
2017-06-08 20:13:12 +0200

    nuevos cambios

---
+++ proxyPattern/src/test/java/org/foobarspam/proxyPattern/ProxyMrMeeseeksTest.java
@@ -0,0 +1,14 @@
+package org.foobarspam.proxyPattern;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProxyMrMeeseeksTest {
+
+	@Test
+	public void createMrMeeseekstest() {
+		
+	}
+
+}
